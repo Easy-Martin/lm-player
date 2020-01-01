cf43c2d5f64400d0046a03e4a1b027e0789f485c
huangjingjing <huangjingjing@netposa.com>
2020-01-01 18:00:43 +0800

    完成重构直播组件

--- src copy/index.js
+++
@@ -1,6 +0,0 @@
-import Player from "./player";
-import HistoryPlayer from "./history";
-import Bar from "./contraller_bar/bar";
-import { createHistoryPlayer, createPlayer } from "./createPlayer";
-
-export { Player as default, Player, HistoryPlayer, createHistoryPlayer, createPlayer, Bar };
