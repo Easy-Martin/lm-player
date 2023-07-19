737f928913d83c05fdc3aa7e5d2f50503de901d2
Martin <253887111@qq.com>
2023-07-19 18:52:50 +0800

    fix: 更新demo

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './lm-player',
   server: {
     proxy: {
       '/api': {
